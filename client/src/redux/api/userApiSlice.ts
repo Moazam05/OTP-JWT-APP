7a139cbe2f183a8701523e0f0ff4e9c42eb67997
Moazam05 <salmanmoazam08@gmail.com>
2023-12-30 19:19:00 +0500

    client setup

---
+++ client/src/redux/api/userApiSlice.ts
@@ -0,0 +1,8 @@
+// Custom Imports
+import { apiSlice } from "./apiSlice";
+
+export const userApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({}),
+});
+
+export const {} = userApiSlice;
